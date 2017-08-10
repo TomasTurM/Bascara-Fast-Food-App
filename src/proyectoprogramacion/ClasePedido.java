0a7c66eeb59167513473378d715176eca5581adc
TomasM <tomasm@tomassystem3.0>
2017-08-10 09:50:39 -0300

    First Commit

---
+++ src/proyectoprogramacion/ClasePedido.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectoprogramacion;
+
+/**
+ *
+ * @author tomasm
+ */
+public class ClasePedido {
+    protected String nombreConsumidor;
+    protected String comida;
+    protected boolean dobleHamburguesa;
+    protected boolean tripleHamburguesa;
+    protected boolean queso;
+    protected boolean tomate1;
+    protected boolean tomate2;
+    protected boolean lechuga;
+    protected boolean mayonesa;
+    protected boolean rucula;
+    protected boolean huevo;
+    protected boolean jamon;
+    protected String cantEmpanadas;
+    protected boolean pollo;
+    protected boolean carne;
+    protected boolean carneCuchillo;
+    protected boolean jamon_queso;
+    protected boolean criollo;
+    protected String bebida;
+    protected String guarnicion;
+    protected String aderezo;
+}
---
+++ src/proyectoprogramacion/Ejecutable.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectoprogramacion;
+
+/**
+ *
+ * @author tomasm
+ */
+public class Ejecutable {
+    public static void main(String[] args) {
+        /*VentanaApp ventanaApp = new VentanaApp();
+        ventanaApp.setVisible(true);*/
+        
+        VentanaLogin ventanaLogin = new VentanaLogin();
+        ventanaLogin.setVisible(true);
+        
+        /*AbrirArchivoTexto text = new AbrirArchivoTexto();
+        text.setVisible(true);*/
+    }
+}
